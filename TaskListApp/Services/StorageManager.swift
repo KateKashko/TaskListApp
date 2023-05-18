7bbfef7bd60fd217bd318b4f33baf42189ef4690
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-05-18 15:43:37 +0300

    Create StorageManager

---
+++ TaskListApp/Services/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  StorageManager.swift
+//  TaskListApp
+//
+//  Created by Kate Kashko on 18.05.2023.
+//
+
+import Foundation
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private init() {}
+}
