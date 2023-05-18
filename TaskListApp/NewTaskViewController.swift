f9302a10286ad68e1374e97ceb18c0f58963d0ee
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-05-18 14:56:18 +0300

    Корректировка кода при помощи ButtonFactory

--- TaskListApp/NewTaskViewController.swift
+++ TaskListApp/NewTaskViewController.swift
@@ -7,6 +7,38 @@
 
 import UIKit
 
+protocol ButtonFactory {
+    func createButton() -> UIButton
+}
+
+final class FilledButtonFactory: ButtonFactory {
+    let title: String
+    let color: UIColor
+    let action: UIAction
+    
+    init(title: String, color: UIColor, action: UIAction) {
+        self.title = title
+        self.color = color
+        self.action = action
+    }
+    func createButton() -> UIButton {
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = color
+        buttonConfiguration.buttonSize = .medium
+        buttonConfiguration.attributedTitle = AttributedString(title, attributes: attributes)
+        
+        let button = UIButton(configuration: buttonConfiguration, primaryAction: action)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+        
+    }
+}
+
+
 final class NewTaskViewController: UIViewController {
     private lazy var taskTextfield: UITextField = { //lazy откладывает инициализацию
         let textField = UITextField()
@@ -17,37 +49,25 @@ final class NewTaskViewController: UIViewController {
     }()
     
     private lazy var saveButton: UIButton = {
-        //Set attributes for button title
-        var attributes = AttributeContainer()
-        attributes.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        //Create button
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.baseBackgroundColor = UIColor(named: "MilkBlue")
-        buttonConfiguration.buttonSize = .medium // это значение по умолчанию, его можно и не указывать
-        buttonConfiguration.attributedTitle = AttributedString("Save Task", attributes: attributes)
-        let button = UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
-            save()
-        })
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+        let filledButtonFactory = FilledButtonFactory(
+            title: "SaveTask",
+            color: UIColor(named: "MilkBlue") ?? .systemBlue,
+            action: UIAction { [unowned self] _ in
+                save()
+            }
+        )
+        return filledButtonFactory.createButton()
     }()
     
     private lazy var cancelButton: UIButton = {
-        //Set attributes for button title
-        var attributes = AttributeContainer()
-        attributes.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        //Create button
-        var buttonConfiguration = UIButton.Configuration.filled()
-        buttonConfiguration.baseBackgroundColor = UIColor(named: "MilkOrange")
-        buttonConfiguration.buttonSize = .medium
-        buttonConfiguration.attributedTitle = AttributedString("Cancel", attributes: attributes)
-        let button = UIButton(configuration: buttonConfiguration, primaryAction: UIAction { [unowned self] _ in
-            dismiss(animated: true)
-        })
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+        let filledButtonFactory = FilledButtonFactory(
+            title: "Cancel",
+            color: UIColor(named: "MilkOrange") ?? .systemRed,
+            action: UIAction { [unowned self] _ in
+                dismiss(animated: true)
+            }
+        )
+        return filledButtonFactory.createButton()
     }()
 
 
